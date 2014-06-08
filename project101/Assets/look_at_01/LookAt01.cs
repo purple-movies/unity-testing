0142d593373e48593cc6ea1cd3f05c75f44fc7bd
purple-movies <ash.rawstron@gmail.com>
2014-06-08 05:24:19 -0700

    Added test for using Transform.lookAt() on parented game objects.

---
+++ project101/Assets/look_at_01/LookAt01.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAt01 : MonoBehaviour {
+
+	public GameObject lookAtGo;
+	Transform lookAtTrans;
+	Vector3 upDirection = new Vector3(0,0,1);
+
+	void Start ()
+	{
+		lookAtTrans = lookAtGo.transform;
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.LookAt( lookAtTrans );	
+	}
+}
---
+++ project101/Assets/look_at_01/Rotate01.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate01 : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.Rotate( 0, 1f, 0 );
+	}
+}
